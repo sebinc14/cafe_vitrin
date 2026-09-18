f422fa5f693986949b2efd0ffc3235b067e16d6a
Sevinç Molla <seviincmolla@gmail.com>
2026-09-18 10:27:35 +0300

    Add remaining customization panels

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4099706448" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3744062393" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
